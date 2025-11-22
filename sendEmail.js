c8aca54dd2ad6d69bbbb994b8ccb8aef02e76ff1
Mondli <mondli46401@gmail.com>
2025-11-22 21:40:21 +0200

    I did the whole thing in 1 hour as I was learning at the same time, not I was using Nodemailer Library from NodeJS and using JavaScript(by Mondli Khoza)

---
+++ sendEmail.js
@@ -0,0 +1,53 @@
+//== Bellow I am importing the Nodemailer library.
+/*= Also importing the dotenv fo that I can access the invironment variables inside this file.
+    the config() will help attack the invironment variables in the .env file*/
+
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const path = require("path");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",  
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.USER, //=== This is the senders gmail address
+    pass: process.env.APP_PASSWORD,//== App password from Gmail account
+  },
+});
+
+const mailOptions ={
+    from: {name: "Frank Mosehla",
+           address: process.env.USER
+    },
+    to: ["mondlik34@gmail.com"],
+    subject: "Send email using nodemailer and gmail ✔",
+    text: `Hi`, // plain‑text body
+    html: `<b>Dear Miss Serathi I hope this message finds you well. 
+           Please find attached our completed Task 3.9 for your 
+           review. https://docs.google.com/presentation/d/13EDBS</b>`, // HTML body
+    attachments: [
+       {
+        filename: "test.pdf",
+        path: path.join(__dirname, "test.pdf"),
+        contentType: 'application/pdf'
+       },
+       {
+        filename: "sample.jpg",
+        path: path.join(__dirname, "sample.jpg"),
+        contentType: 'image.jpg'
+       }
+    ]
+}
+
+const sendEmail = async (transporter, mailOptions)=>{
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log(`Email has been sent to ${mailOptions.to}`);
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+sendEmail(transporter, mailOptions);
